26a4e73656ddbf477fd9b7e67aea03d63998c2ff
Corgi <boy860121@gmail.com>
2022-10-31 15:03:46 +0800

    Change Path

--- Package.swift
+++ Package.swift
@@ -23,7 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FIDO_Passkey",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/PasskeysManger"
+        ),
         .testTarget(
             name: "FIDO_PasskeyTests",
             dependencies: ["FIDO_Passkey"]),
